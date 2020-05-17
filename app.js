6ab51621b0b6e16cc9b56d6a943be845d6d01452
shiva <shiva@shiva>
2020-05-17 19:51:55 +0530

    authentication using environment variable

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const express= require("express");
 const bodyParser=require("body-parser");
 const ejs= require("ejs");
@@ -17,7 +18,7 @@ const userSchema= new mongoose.Schema({
   password:String
 });
 
-const secret="encryptusingmongoose-encryption";
+const secret=process.env.SECRET;
 userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
 
 const User=mongoose.model("User", userSchema);
